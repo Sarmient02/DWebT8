7840a311de50eed001be601a36e049f1bcf746e6
Sarmient02 <javiersarmiento02@gmail.com>
2022-08-19 00:28:32 -0500

    Update settings.py

--- DWebT8/settings.py
+++ DWebT8/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'personas.apps.PersonasConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'DWebT8.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'DWebT8.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'ijjnhodu',
+        'USER': 'ijjnhodu',
+        'PASSWORD': 'SfyAPnYEv4KdjcgLuyhDjaQJuf0A2Acv',
+        'HOST': 'suleiman.db.elephantsql.com',
+        'PORT': '5432'
     }
 }
 
