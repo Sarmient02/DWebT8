57a9dabb91c5aeaae36fa0d10ea61cffc11fc13f
Sarmient02 <javiersarmiento02@gmail.com>
2022-08-22 18:06:32 -0500

    Update urls.py

--- personas/urls.py
+++ personas/urls.py
@@ -7,5 +7,13 @@ urlpatterns = [
     path('documentos/', views.documentos, name='documentos'),
     path('documentos/new/', views.new_document, name='new_document'),
     path('documentos/edit/<pk>', views.edit_document, name='edit_document'),
-    path('documentos/delete/<pk>', views.delete_document, name='delete_document')
+    path('documentos/delete/<pk>', views.delete_document, name='delete_document'),
+    path('ciudades/', views.ciudades, name='ciudades'),
+    path('ciudades/new/', views.new_ciudad, name='new_ciudad'),
+    path('ciudades/edit/<pk>', views.edit_ciudad, name='edit_ciudad'),
+    path('ciudades/delete/<pk>', views.delete_ciudad, name='delete_ciudad'),
+    path('personas/', views.personas, name='personas'),
+    path('personas/new/', views.new_persona, name='new_persona'),
+    path('personas/edit/<pk>', views.edit_persona, name='edit_persona'),
+    path('personas/delete/<pk>', views.delete_persona, name='delete_persona')
 ]
